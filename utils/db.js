14855c3c3263b1e5b327835940138130c3a8cd76
Davinder-kumar-ship-it <engdavinder13@gmail.com>
2026-05-17 15:01:43 +0530

    new changes in database file

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,6 @@
 import { connect } from 'mongoose';
-
+import dotenv from 'dotenv';
+dotenv.config(); 
 const databaseConnection = () => {
     connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
